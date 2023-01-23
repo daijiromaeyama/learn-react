8b9b589dddc5acaf8f8b6d3eb940572071a90b14
daijiromaeyama <daijiro.maeyama13@gmail.com>
2023-01-23 22:08:03 +0900

    Add render_and_commit directory

---
+++ src/pages/adding_interactivity/render_and_commit/gallery.tsx
@@ -0,0 +1,12 @@
+import Image from './image'
+
+export default function Gallery(): JSX.Element {
+  return (
+    <section>
+      <h1>Inspireing Sculptures</h1>
+      <Image />
+      <Image />
+      <Image />
+    </section>
+  )
+}
---
+++ src/pages/adding_interactivity/render_and_commit/image.tsx
@@ -0,0 +1,8 @@
+export default function Image():JSX.Element {
+  return (
+    <img
+      src='https://i.imgur.com/ZF6s192.jpg'
+      alt="'Floralis Generica' by Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals"
+    />
+  )
+}
